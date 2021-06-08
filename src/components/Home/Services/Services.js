8641caa6746f62f5bc3fa51e1e52c47cd26dc926
shahnewaz601 <shahnewaz601@gmail.com>
2021-06-08 18:28:10 +0600

    update spinner

--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -6,17 +6,20 @@ const Services = () => {
     const [services, setServices] = useState([]);
     const [visible, setVisible] = useState(3);
     const allServices = services.slice(0, visible);
+    const [loading, setLoading] = useState(false);
 
     const ViewMoreServices =  () => {
         setVisible(previousLeague => previousLeague + 3)
     };
 
     useEffect(() => {
+        setLoading(true)
         fetch("https://powerful-brushlands-39960.herokuapp.com/services")
         .then(res => res.json())
         .then(data => {
             setServices(data);
         })
+        .then(() => setLoading(false))
         .catch(error => alert("Something went wrong!! Please try again later!"))
     }, [])
 
@@ -26,6 +29,13 @@ const Services = () => {
             <div className="sectionTitle">
                 <h2 className="text-center pt-5 mb-5 section-title">Services</h2>
             </div>
+            <div className="text-center mt-4">
+                    {loading && (
+                        <div class="spinner-border text-primary" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                    )}
+                </div>
             <div className="row">
                {
                    allServices.map(service => <Service service={service} key={service._id}></Service>)

6155429da94361bef6985949f24ea1bd49337b97
shahnewaz601 <shahnewaz601@gmail.com>
2021-06-08 14:07:17 +0600

    dynamic contact form using email.js

--- src/components/Home/Contact/Contact.js
+++ src/components/Home/Contact/Contact.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import emailjs from 'emailjs-com';
 import './Contact.css';
 const Contact = () => {
+    function sendEmail(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_200r93a', 'template_p0s2zut', e.target, 'user_hJSU6AMqoIqz90tZfOa1r')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset();
+    }
+
     return (
         <section className="container mb-5">
       <div className="contact-section">
@@ -10,22 +23,22 @@ const Contact = () => {
         <div className="row mb-5">
             <div className="col-md-2"></div>
             <div className="col-md-8">
-                <form>
+                <form onSubmit={sendEmail}>
                     <div className="row">
                         <div className="col-12 col-md-6 form-group mb-4">
-                            <input type="text" className="form-control" placeholder="Full Name"/>
+                            <input type="name" name="name" className="form-control" placeholder="Full Name" required/>
                         </div>
                         <div className="col-12 col-md-6 form-group mb-4">
-                            <input type="email" name="email" className="form-control" placeholder="Email Address"/>
+                            <input type="email" name="email" className="form-control" placeholder="Email Address" required/>
                         </div>
                         <div className="col-12 form-group mb-4">
-                            <input type="number" name="subject" className="form-control" placeholder="Phone Number"/>
+                            <input type="number" name="number" className="form-control" placeholder="Phone Number"/>
                         </div>
                         <div className="col-12 form-group mb-4">
-                            <textarea className="form-control" name="message"  rows="5" placeholder="Your Message"></textarea>
+                            <textarea className="form-control" name="message"  rows="5" placeholder="Your Message" required></textarea>
                         </div>
                         <div className="col-8 col-sm-5 col-md-4 contact-btn mb-5 pb-2">
-                            <Link to="#" className="btn btn-style px-3 mt-4  fw-bold text-white ms-3">Send Message</Link>
+                            <button type="submit" className="btn btn-style px-3 mt-4  fw-bold text-white ms-3">Send Message</button>
                         </div>
                     </div>
                 </form>

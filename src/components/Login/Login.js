6562ea5cc946c4cca78f6af19bd49ccd9d36a22c
shahnewaz601 <shahnewaz601@gmail.com>
2021-09-12 14:37:54 +0600

    separate login and signup field

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -26,65 +26,65 @@ const Login = () => {
         success: false,
         newUser: false
     })
-    
+
     const history = useHistory();
     const location = useLocation();
     const { from } = location.state || { from: { pathname: "/" } };
 
     const googleSignIn = () => {
         handleGoogleSignIn()
-        .then(res => {
-           if(res){
-                setLoading(true);
-                history.replace(from);
-           }
-        })
+            .then(res => {
+                if (res) {
+                    setLoading(true);
+                    history.replace(from);
+                }
+            })
     }
 
     const facebookSignIn = () => {
         handleFacebookSignIn()
-        .then(res => {
-            if(res){
-                setLoading(true);
-                history.replace(from);
-            }
-        })
+            .then(res => {
+                if (res) {
+                    setLoading(true);
+                    history.replace(from);
+                }
+            })
     }
-    
+
     const handleBlur = (e) => {
         let isFieldValid = true;
 
-        if(isFieldValid){
-            const newUserInfo = {...user};
+        if (isFieldValid) {
+            const newUserInfo = { ...user };
             newUserInfo[e.target.name] = e.target.value;
             setUser(newUserInfo);
         }
     }
 
     const onSubmit = (e) => {
-        if(newUser && user.email && user.password){
+        if (newUser && user.email && user.password) {
             createUserWithEmailAndPassword(user.email, user.password)
-            .then(res => {
-                setUser(res);
-            })
+                .then(res => {
+                    setUser(res);
+                })
         }
-       else if(!newUser && user.email && user.password){
+        else if (!newUser && user.email && user.password) {
             signInWithEmailAndPassword(user.email, user.password)
-            .then( res => {
-                storeUserInfo(res);
-                setLoading(true);
-                history.replace(from);
-            })
+                .then(res => {
+                    storeUserInfo(res);
+                    setLoading(true);
+                    history.replace(from);
+                })
         }
     }
-    
+
     return (
-        <>    
+        <>
             {loading && <Loading />}
             <div className="pt-3">
                 <div className="container">
                     <Link to="/" className="navbar-brand web-name">
-                        <img src={logo} alt=""/>
+                        <img src={logo} alt="" />
                     </Link>
                 </div>
             </div>
@@ -94,90 +94,101 @@ const Login = () => {
                     <div className="mb-3 text-center">
                         {
                             newUser ? <h4>Create an account</h4> :
-                            <h4>Login</h4>
+                                <h4>Login</h4>
                         }
                     </div>
                     <form onSubmit={handleSubmit(onSubmit)}>
-                        {
-                            newUser && (
+                        {/* Signup form */}
+                        {newUser &&
+                            <>
                                 <div className="form-group  mb-3">
                                     <label htmlFor="name">Name</label>
-                                    <input type="name" onBlur={handleBlur} ref={register({ required: true, minLength: 4 })} className="form-control" name="name" id="name"/>
+                                    <input type="name" onBlur={handleBlur} ref={register({ required: true, minLength: 4 })} className="form-control" name="name" id="name" />
                                     {errors.name && errors.name.type === "required" && <p>Please enter your name</p>}
                                     {errors.name && errors.name.type === "minLength" && <p>Please enter at least 4 characters</p>}
                                 </div>
-                            )
-                        }
-                        <div className="form-group  mb-3">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" onBlur={handleBlur} className="form-control" name="email" id="email" ref={register({ required: true, pattern: /\S+@\S+\.\S+/})}/>
-                            {errors.email && errors.email.type === "required" && <p>Please enter your email address</p>}
-                            {errors.email && errors.email.type === "pattern" && <p>Please enter the valid email address</p>}
-                        </div>
-                        <div className="form-group  mb-3">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" onBlur={handleBlur} ref={register({ required: true, minLength: 6 })} className="form-control" name="password" id="password"/>
-                            {errors.password && errors.password.type === "required" && <p>Please enter your password</p>}
-                               {errors.password && errors.password.type === "minLength" && <p>Please enter at least 6 characters</p>}
-                        </div>
-                        {
-                            newUser && (
+                                <div className="form-group  mb-3">
+                                    <label htmlFor="email">Email</label>
+                                    <input type="email" onBlur={handleBlur} className="form-control" name="email" id="email" ref={register({ required: true, pattern: /\S+@\S+\.\S+/ })} />
+                                    {errors.email && errors.email.type === "required" && <p>Please enter your email address</p>}
+                                    {errors.email && errors.email.type === "pattern" && <p>Please enter the valid email address</p>}
+                                </div>
+                                <div className="form-group  mb-3">
+                                    <label htmlFor="password">Password</label>
+                                    <input type="password" onBlur={handleBlur} ref={register({ required: true, minLength: 6 })} className="form-control" name="password" id="password" />
+                                    {errors.password && errors.password.type === "required" && <p>Please enter your password</p>}
+                                    {errors.password && errors.password.type === "minLength" && <p>Please enter at least 6 characters</p>}
+                                </div>
                                 <div className="form-group mb-1">
                                     <label htmlFor="password_confirm">Confirm Password</label>
-                                    <input type="password" onBlur={handleBlur} ref={register({ required: true,
+                                    <input type="password" onBlur={handleBlur} ref={register({
+                                        required: true,
                                         validate: (value) => value === password.current
-                                    })}  className="form-control" id="password_confirm" name="password_confirm"/>
+                                    })} className="form-control" id="password_confirm" name="password_confirm" />
                                     {errors.password_confirm && errors.password_confirm.type === "required" && <p>This field is required</p>}
                                     {errors.password_confirm && errors.password_confirm.type === "validate" && <p>Password does not match</p>}
                                 </div>
-                            )
+                            </>
                         }
-                        {
-                            !newUser && (
+                        {/* Login form */}
+                        {!newUser &&
+                            <>
+                                <div className="form-group  mb-3">
+                                    <label htmlFor="email">Email</label>
+                                    <input type="email" onBlur={handleBlur} className="form-control" name="email" id="email" ref={register({ required: true, pattern: /\S+@\S+\.\S+/ })} />
+                                    {errors.email && errors.email.type === "required" && <p>Please enter your email address</p>}
+                                    {errors.email && errors.email.type === "pattern" && <p>Please enter the valid email address</p>}
+                                </div>
+                                <div className="form-group  mb-1">
+                                    <label htmlFor="password">Password</label>
+                                    <input type="password" onBlur={handleBlur} ref={register({ required: true, minLength: 6 })} className="form-control" name="password" id="password" />
+                                    {errors.password && errors.password.type === "required" && <p>Please enter your password</p>}
+                                    {errors.password && errors.password.type === "minLength" && <p>Please enter at least 6 characters</p>}
+                                </div>
                                 <div className="row form-group mb-3">
                                     <div className="col-6">
                                         <input type="checkbox" name="remember" id="remember" />
                                         <label htmlFor="remember" className="ms-2">Remember Me</label>
                                     </div>
                                     <div className="col-6 text-right forget">
-                                        <p className="text-color" style={{textAlign: 'end'}}>Forget Password</p>
+                                        <p className="text-color" style={{ textAlign: 'end' }}>Forget Password</p>
                                     </div>
                                 </div>
-                                )
+                            </>
                         }
                         <div className="form-group mt-3">
                             <input type="submit" className="btn col-12 btn-style" value={newUser ? 'Create an account' : 'Login'} />
                             {user.error && <p className="text-danger mt-2">{user.error}</p>}
-                            {user.success && <p className="success-alert mt-2">{newUser ?'Account created successfully' : console.log('successfully logged')}</p>}
+                            {user.success && <p className="success-alert mt-2">{newUser ? 'Account created successfully' : console.log('successfully logged')}</p>}
                         </div>
                         {
                             newUser ? <p className="text-center mt-3">Already have an account? <span onClick={() => setNewUser(!newUser)} className="link-span">Login</span></p> :
-                            <p className="text-center mt-3">Don't have an account? <span onClick={() => setNewUser(!newUser)} className="link-span">Create an account</span></p>
+                                <p className="text-center mt-3">Don't have an account? <span onClick={() => setNewUser(!newUser)} className="link-span">Create an account</span></p>
                         }
                     </form>
                 </div>
 
                 <div className="extra-info  mb-5">
-                        <div className="or mb-3">
-                            <div className="col-4 border-bottom"></div>
-                            <div className="px-3">
-                                Or
-                            </div>
-                            <div className="col-4 border-bottom"></div>
+                    <div className="or mb-3">
+                        <div className="col-4 border-bottom"></div>
+                        <div className="px-3">
+                            Or
                         </div>
-                        <div className="social-link">
-                            <button onClick={googleSignIn} className="btn btn-facebook mb-2">
-                                <FontAwesomeIcon className="me-2" icon={faGoogle} />
-                                Continue with Google
-                                </button>
-                            <button onClick={facebookSignIn} className="btn btn-facebook">
-                                <FontAwesomeIcon className="me-2" icon={faFacebook} />
-                                Continue with Facebook
-                            </button>
-                        </div>
+                        <div className="col-4 border-bottom"></div>
                     </div>
+                    <div className="social-link">
+                        <button onClick={googleSignIn} className="btn btn-facebook mb-2">
+                            <FontAwesomeIcon className="me-2" icon={faGoogle} />
+                            Continue with Google
+                        </button>
+                        <button onClick={facebookSignIn} className="btn btn-facebook">
+                            <FontAwesomeIcon className="me-2" icon={faFacebook} />
+                            Continue with Facebook
+                        </button>
+                    </div>
+                </div>
             </div>
-        </>    
+        </>
     );
 };
 
